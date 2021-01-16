b6b0a80d9d77e5feb4e66f384622fc70dd961588
jjggzz <1945282561@qq.com>
2021-01-16 16:42:53 +0800

    安装文件

--- wininstall.bat
+++ wininstall.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 go get -u github.com/go-sql-driver/mysql@v1.5.0
 go get -u github.com/kevinburke/go-bindata/go-bindata@v3.22.0+incompatible
-go install github.com/jjggzz/generate
+go install github.com/jjggzz/generate/cmd
 @ECHO OFF
 
