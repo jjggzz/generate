d7fcfc93e0fdea5f5b5b4ba52df6a0335c132810
jjggzz <1945282561@qq.com>
2021-01-20 17:41:46 +0800

    条件查询between,not between完成

--- repository/repo_test.go
+++ repository/repo_test.go
@@ -18,7 +18,7 @@ func Test_get(t *testing.T) {
 
 	repo := NewRepo(db)
 
-	list, err := repo.CustomerRepo.SelectByExample(new(CustomerExample).AndAccessKeyIn([]string{"ehuDIF", "oRtpuc"}).AndEmailIsNotNull())
+	list, err := repo.CustomerRepo.SelectByExample(new(CustomerExample).AndAccessKeyIn([]string{"ehuDIF", "oRtpuc"}).AndIdNotBetween(1638, 1639))
 	if err != nil {
 		panic(err)
 		return
