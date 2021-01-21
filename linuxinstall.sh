3ae97644a46398388e87baa5fc363988fab1c66f
jjggzz <1945282561@qq.com>
2021-01-21 16:39:08 +0800

    添加clear方法

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 go get -u github.com/go-sql-driver/mysql@v1.5.0
 go get -u github.com/kevinburke/go-bindata/go-bindata@v3.22.0+incompatible
-go install github.com/jjggzz/generate/cmd
+go install github.com/jjggzz/generate/cmd/sqlxgen
